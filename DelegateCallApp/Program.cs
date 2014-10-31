52f21f285819a11fd25b68045e9958476076058f
s.degambe@gmail.com <Sebad#21>
2014-10-31 22:36:11 +0100

    description changed

--- DelegateCallApp/Program.cs
+++ DelegateCallApp/Program.cs
@@ -7,7 +7,7 @@ namespace DelegateCallApp
         static void Main()
         {
             var dc = new DelegateClass();
-            dc.GetPrivateMethodWithParameter()("seba1");
+            dc.GetPrivateMethodWithParameter()("It's work");
             dc.GetPrivateMethodWithoutParameter()();
             Console.WriteLine(dc.GetReturnValueMethod()(false));
             Console.ReadLine();
